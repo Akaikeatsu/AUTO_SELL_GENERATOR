6110916d68323ff46dff02907bbb757ea6ceaebb
Akaikeatsu <adofomezaromero@hotmail.com>
2018-05-13 10:47:54 -0500

    Oreder viewer

--- ASG/src/asg/tools/JDBSConectionTools.java
+++ ASG/src/asg/tools/JDBSConectionTools.java
@@ -125,11 +125,12 @@ public class JDBSConectionTools {
     */
     public Connection ConnectionTool (){ // Ofrece una manera de acceder a la base de datos que sea necesario en el momento. 
                                          // Dado que se accede a la base de datos de la aplicación y a la de la empresa.
-        String connectString = "jdbc:postgresql:"+RecoverDirection();
+        /*String connectString = "jdbc:postgresql:"+RecoverDirection();
         String user = RecoverUser();
-        String pass = RecoverPass();
+        String pass = RecoverPass();*/
         try {
-            MyConnection = DriverManager.getConnection(connectString, user, pass);
+            /*MyConnection = DriverManager.getConnection(connectString, user, pass);*/
+            MyConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ASG_DB","postgres","no32sdc1");
         } catch (Exception e) {
         }
         return MyConnection;
